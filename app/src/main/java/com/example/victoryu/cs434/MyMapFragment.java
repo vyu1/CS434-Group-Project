d4c305665f95a62a4a32121350ce91eaf0ea6b37
Victor Yu <victorseungleeyu@gmail.com>
2015-12-07 18:16:41 -0500

    Made personal map bubbles all one color

--- app/src/main/java/com/example/victoryu/cs434/MyMapFragment.java
+++ app/src/main/java/com/example/victoryu/cs434/MyMapFragment.java
@@ -39,40 +39,40 @@ public class MyMapFragment extends Fragment {
             map.setMyLocationEnabled(true);
 
             int smallBubbleSize = getResources().getDimensionPixelSize(R.dimen.small_map_marker_size);
-            Bitmap blueMarkerBitmap = Bitmap.createBitmap(smallBubbleSize, smallBubbleSize, Bitmap.Config.ARGB_8888);
-            Canvas canvas = new Canvas(blueMarkerBitmap);
-            Drawable shape = getResources().getDrawable(R.drawable.blue_bubble);
-            shape.setBounds(0, 0, blueMarkerBitmap.getWidth(), blueMarkerBitmap.getHeight());
+            Bitmap smallBubble = Bitmap.createBitmap(smallBubbleSize, smallBubbleSize, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(smallBubble);
+            Drawable shape = getResources().getDrawable(R.drawable.red_bubble);
+            shape.setBounds(0, 0, smallBubble.getWidth(), smallBubble.getHeight());
             shape.draw(canvas);
 
             int mediumBubbleSize = getResources().getDimensionPixelSize(R.dimen.medium_map_marker_size);
-            Bitmap orangeMarkerBitmap = Bitmap.createBitmap(mediumBubbleSize, mediumBubbleSize, Bitmap.Config.ARGB_8888);
-            Canvas canvas2 = new Canvas(orangeMarkerBitmap);
-            Drawable shape2 = getResources().getDrawable(R.drawable.orange_bubble);
-            shape2.setBounds(0, 0, orangeMarkerBitmap.getWidth(), orangeMarkerBitmap.getHeight());
+            Bitmap mediumBubble = Bitmap.createBitmap(mediumBubbleSize, mediumBubbleSize, Bitmap.Config.ARGB_8888);
+            Canvas canvas2 = new Canvas(mediumBubble);
+            Drawable shape2 = getResources().getDrawable(R.drawable.red_bubble);
+            shape2.setBounds(0, 0, mediumBubble.getWidth(), mediumBubble.getHeight());
             shape2.draw(canvas2);
 
             int largeBubbleSize = getResources().getDimensionPixelSize(R.dimen.large_map_marker_size);
-            Bitmap redMarkerBitmap = Bitmap.createBitmap(largeBubbleSize, largeBubbleSize, Bitmap.Config.ARGB_8888);
-            Canvas canvas3 = new Canvas(redMarkerBitmap);
+            Bitmap largeBubble = Bitmap.createBitmap(largeBubbleSize, largeBubbleSize, Bitmap.Config.ARGB_8888);
+            Canvas canvas3 = new Canvas(largeBubble);
             Drawable shape3 = getResources().getDrawable(R.drawable.red_bubble);
-            shape3.setBounds(0, 0, redMarkerBitmap.getWidth(), redMarkerBitmap.getHeight());
+            shape3.setBounds(0, 0, largeBubble.getWidth(), largeBubble.getHeight());
             shape3.draw(canvas3);
 
             LatLng mckeldin = new LatLng(38.985986, -76.945110);
-            map.addMarker(new MarkerOptions().position(mckeldin).title("McKeldin").icon(BitmapDescriptorFactory.fromBitmap(redMarkerBitmap)));
+            map.addMarker(new MarkerOptions().position(mckeldin).title("McKeldin").icon(BitmapDescriptorFactory.fromBitmap(largeBubble)));
             map.moveCamera(CameraUpdateFactory.newLatLngZoom(mckeldin, 16.0f));
 
             LatLng business = new LatLng(38.983107, -76.947447);
-            map.addMarker(new MarkerOptions().position(business).title("Smith").icon(BitmapDescriptorFactory.fromBitmap(blueMarkerBitmap)));
+            map.addMarker(new MarkerOptions().position(business).title("Smith").icon(BitmapDescriptorFactory.fromBitmap(smallBubble)));
             map.moveCamera(CameraUpdateFactory.newLatLngZoom(business, 16.0f));
 
             LatLng tydings = new LatLng(38.984758, -76.944056);
-            map.addMarker(new MarkerOptions().position(tydings).title("Tydings").icon(BitmapDescriptorFactory.fromBitmap(orangeMarkerBitmap)));
+            map.addMarker(new MarkerOptions().position(tydings).title("Tydings").icon(BitmapDescriptorFactory.fromBitmap(mediumBubble)));
             map.moveCamera(CameraUpdateFactory.newLatLngZoom(tydings, 16.0f));
 
             LatLng home = new LatLng(38.982411, -76.943272);
-            map.addMarker(new MarkerOptions().position(home).title("Home").icon(BitmapDescriptorFactory.fromBitmap(redMarkerBitmap)));
+            map.addMarker(new MarkerOptions().position(home).title("Home").icon(BitmapDescriptorFactory.fromBitmap(largeBubble)));
 
             map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
                 @Override
