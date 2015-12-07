54003ac55c0928c8ad0d9396c312bf707dcefe8e
Victor Yu <victorseungleeyu@gmail.com>
2015-12-07 18:12:25 -0500

    Implemented save button in EditActivity

--- app/src/main/java/com/example/victoryu/cs434/EditActivity.java
+++ app/src/main/java/com/example/victoryu/cs434/EditActivity.java
@@ -1,7 +1,10 @@
 package com.example.victoryu.cs434;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -13,5 +16,9 @@ public class EditActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+    }
+
+    public void saveInformation(View saveButton) {
+        onBackPressed();
     }
 }
